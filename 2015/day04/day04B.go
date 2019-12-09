6345b6fb0423116535f3fe85c5087db406c698bb
Sergej Voronov <sergej.voronov@geniussports.com>
2019-12-10 01:34:00 +0200

    Add Day 04 solution

---
+++ 2015/day04/day04B.go
@@ -0,0 +1,24 @@
+package day04
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func SolveB(input string) string {
+	var answer int
+	input = strings.TrimSpace(input)
+	for {
+		str := strconv.Itoa(answer)
+		hash := md5.Sum([]byte(input + str))
+		if strings.Compare(hex.EncodeToString(hash[:])[:6], "000000") == 0 {
+			break
+		}
+		answer++
+	}
+
+	return fmt.Sprintf("Day 04(B) answer: %d", answer)
+}
--- 2015/main.go
+++ 2015/main.go
@@ -5,16 +5,20 @@ import (
 	"github.com/sergejvoronov/adventofcode/2015/day01"
 	"github.com/sergejvoronov/adventofcode/2015/day02"
 	"github.com/sergejvoronov/adventofcode/2015/day03"
+	"github.com/sergejvoronov/adventofcode/2015/day04"
 )
 
 func main() {
-	// DAY 01
-	println(day01.SolveA(adventofcode.ReadInputData("inputs/day01.input")))
-	println(day01.SolveB(adventofcode.ReadInputData("inputs/day01.input")))
-	// DAY 02
-	println(day02.SolveA(adventofcode.ReadInputData("inputs/day02.input")))
-	println(day02.SolveB(adventofcode.ReadInputData("inputs/day02.input")))
+	// DAY 04
+	println(day04.SolveA(adventofcode.ReadInputData("inputs/day04.input")))
+	println(day04.SolveB(adventofcode.ReadInputData("inputs/day04.input")))
 	// DAY 03
 	println(day03.SolveA(adventofcode.ReadInputData("inputs/day03.input")))
 	println(day03.SolveB(adventofcode.ReadInputData("inputs/day03.input")))
+	// DAY 02
+	println(day02.SolveA(adventofcode.ReadInputData("inputs/day02.input")))
+	println(day02.SolveB(adventofcode.ReadInputData("inputs/day02.input")))
+	// DAY 01
+	println(day01.SolveA(adventofcode.ReadInputData("inputs/day01.input")))
+	println(day01.SolveB(adventofcode.ReadInputData("inputs/day01.input")))
 }
