f86c360476a8bae09ca8cdb737203b4745f1fe77
Sergej Voronov <sergej@voronov.lt>
2021-12-01 23:50:07 +0200

    Add 2020 day 3 solution

--- internal/year/2020/day03.go
+++ internal/year/2020/day03.go
@@ -1,6 +1,9 @@
 package aoc2020
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/sergejvoronov/advent-of-code/internal/solution"
 )
 
@@ -11,9 +14,81 @@ func Day03() solution.Provider {
 }
 
 func (*day03) SolveA(input string) string {
-	return solution.NotImplemented
+	var (
+		answer int
+
+		tree = "#"
+
+		right, down = 3, 1
+		x, y        = 0, 0
+	)
+
+	grid := strings.Split(input, "\n")
+	grid = grid[:len(grid)-1]
+
+	xAxisLength := len(grid[0])
+	yAxisLength := len(grid)
+
+	for range grid {
+		y += down
+		x += right
+		if x > xAxisLength-1 {
+			x -= xAxisLength
+		}
+		if string(grid[y][x]) == tree {
+			answer++
+		}
+		if yAxisLength-1 == y {
+			break
+		}
+	}
+
+	return strconv.Itoa(answer)
 }
 
 func (*day03) SolveB(input string) string {
-	return solution.NotImplemented
+	var (
+		answer int = 1
+		tree       = "#"
+	)
+
+	type slope struct {
+		right, down int
+	}
+
+	slopes := []slope{
+		{right: 1, down: 1},
+		{right: 3, down: 1},
+		{right: 5, down: 1},
+		{right: 7, down: 1},
+		{right: 1, down: 2},
+	}
+
+	grid := strings.Split(input, "\n")
+	grid = grid[:len(grid)-1]
+
+	xAxisLength := len(grid[0])
+	yAxisLength := len(grid)
+
+	for _, slope := range slopes {
+		trees := 0
+		x, y := 0, 0
+		for range grid {
+			y += slope.down
+			x += slope.right
+			if x > xAxisLength-1 {
+				x -= xAxisLength
+			}
+			if string(grid[y][x]) == tree {
+				trees++
+			}
+			if yAxisLength-1 == y {
+				break
+			}
+		}
+		answer *= trees
+	}
+
+	return strconv.Itoa(answer)
+
 }
