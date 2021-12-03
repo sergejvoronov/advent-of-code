91b0a56ef6b2ee2301fd392e24df62326105c1e2
Sergej Voronov <sergej@voronov.lt>
2021-12-03 11:53:35 +0200

    Add 2021 day 3 A solution

--- internal/year/2021/day03.go
+++ internal/year/2021/day03.go
@@ -1,6 +1,9 @@
 package aoc2021
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/sergejvoronov/advent-of-code/internal/solution"
 )
 
@@ -11,7 +14,37 @@ func Day03() solution.Provider {
 }
 
 func (*day03) SolveA(input string) string {
-	return solution.NotImplemented
+	type report struct {
+		gamma, epsilon string
+	}
+
+	numbers := strings.Split(input, "\n")
+	r := report{}
+
+	for x := 0; x < len(numbers[0]); x++ {
+		zeros, ones := 0, 0
+		for _, number := range numbers {
+			if string(number[x]) == "0" {
+				zeros++
+				continue
+			}
+			ones++
+		}
+
+		if ones > zeros {
+			r.gamma += "0"
+			r.epsilon += "1"
+			continue
+		}
+		r.gamma += "1"
+		r.epsilon += "0"
+
+	}
+
+	gamma, _ := strconv.ParseInt(r.gamma, 2, 64)
+	epsilon, _ := strconv.ParseInt(r.epsilon, 2, 64)
+
+	return strconv.Itoa(int(gamma) * int(epsilon))
 }
 
 func (*day03) SolveB(input string) string {
