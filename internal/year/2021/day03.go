58ff13dcbc2355ca7ea5a68028c5fffffac9b943
Sergej Voronov <sergej@voronov.lt>
2021-12-03 12:32:51 +0200

    Add 2021 day 3B solution

--- internal/year/2021/day03.go
+++ internal/year/2021/day03.go
@@ -48,5 +48,69 @@ func (*day03) SolveA(input string) string {
 }
 
 func (*day03) SolveB(input string) string {
-	return solution.NotImplemented
+	type report struct {
+		oxygenGenerator, co2scrubber string
+	}
+
+	numbers := strings.Split(input, "\n")
+	r := report{}
+
+	oxygenFilteredNumbers, co2scrubberFilteredNumbers := numbers, numbers
+
+	for x := 0; x < len(numbers[0]); x++ {
+		zeros, ones := 0, 0
+		var zeroNumbers, oneNumbers []string
+
+		for _, number := range oxygenFilteredNumbers {
+			if string(number[x]) == "0" {
+				zeros++
+				zeroNumbers = append(zeroNumbers, number)
+				continue
+			}
+			ones++
+			oneNumbers = append(oneNumbers, number)
+		}
+
+		if ones > zeros || ones == zeros {
+			oxygenFilteredNumbers = oneNumbers
+		} else {
+			oxygenFilteredNumbers = zeroNumbers
+		}
+
+		if len(oxygenFilteredNumbers) == 1 {
+			r.oxygenGenerator = oxygenFilteredNumbers[0]
+			break
+		}
+	}
+
+	for x := 0; x < len(numbers[0]); x++ {
+		zeros, ones := 0, 0
+		var zeroNumbers, oneNumbers []string
+
+		for _, number := range co2scrubberFilteredNumbers {
+			if string(number[x]) == "0" {
+				zeros++
+				zeroNumbers = append(zeroNumbers, number)
+				continue
+			}
+			ones++
+			oneNumbers = append(oneNumbers, number)
+		}
+
+		if ones > zeros || ones == zeros {
+			co2scrubberFilteredNumbers = zeroNumbers
+		} else {
+			co2scrubberFilteredNumbers = oneNumbers
+		}
+
+		if len(co2scrubberFilteredNumbers) == 1 {
+			r.co2scrubber = co2scrubberFilteredNumbers[0]
+			break
+		}
+	}
+
+	oxygenGenerator, _ := strconv.ParseInt(r.oxygenGenerator, 2, 64)
+	co2scrubber, _ := strconv.ParseInt(r.co2scrubber, 2, 64)
+
+	return strconv.Itoa(int(oxygenGenerator) * int(co2scrubber))
 }
