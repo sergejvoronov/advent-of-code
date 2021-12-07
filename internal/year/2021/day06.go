11f2d4060c6d0902dcaee735924ca9b4e20227f5
Sergej Voronov <sergej@voronov.lt>
2021-12-07 02:21:41 +0200

    Add 2021 day 6 solution

--- internal/year/2021/day06.go
+++ internal/year/2021/day06.go
@@ -1,6 +1,9 @@
 package aoc2021
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/sergejvoronov/advent-of-code/internal/solution"
 )
 
@@ -10,10 +13,42 @@ func Day06() solution.Provider {
 	return &day06{}
 }
 
-func (*day06) SolveA(input string) string {
-	return solution.NotImplemented
+func (d *day06) SolveA(input string) string {
+	const cycle = 80
+
+	return strconv.Itoa(d.spawnFishes(input, cycle))
 }
 
-func (*day06) SolveB(input string) string {
-	return solution.NotImplemented
+func (d *day06) SolveB(input string) string {
+	const cycle = 256
+
+	return strconv.Itoa(d.spawnFishes(input, cycle))
+}
+
+func (d *day06) spawnFishes(input string, cycle int) (fishCount int) {
+	timers := strings.Split(input, ",")
+	fishes := make([]int, 0)
+	for _, t := range timers {
+		timer, _ := strconv.Atoi(t)
+		fishes = append(fishes, timer)
+	}
+
+	days := make([]int, 9)
+	for _, fish := range fishes {
+		days[fish]++
+	}
+	for i := 0; i < cycle; i++ {
+		first := days[0]
+		for i := 0; i < 8; i++ {
+			days[i] = days[i+1]
+		}
+		days[6] += first
+		days[8] = first
+	}
+
+	for _, fish := range days {
+		fishCount += fish
+	}
+
+	return fishCount
 }
