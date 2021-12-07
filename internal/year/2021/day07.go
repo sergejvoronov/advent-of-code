34b55097e5b7c2e39573e15ad2ce569d2946fa0a
Sergej Voronov <sergej@voronov.lt>
2021-12-07 15:00:08 +0200

    Add 2021 day 7 solution

--- internal/year/2021/day07.go
+++ internal/year/2021/day07.go
@@ -1,6 +1,11 @@
 package aoc2021
 
 import (
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+
 	"github.com/sergejvoronov/advent-of-code/internal/solution"
 )
 
@@ -11,9 +16,74 @@ func Day07() solution.Provider {
 }
 
 func (*day07) SolveA(input string) string {
-	return solution.NotImplemented
+	content := strings.Split(input, ",")
+
+	var (
+		requiredFuel, median int
+		positions            = make([]int, 0, len(content))
+	)
+
+	// Convert to ints
+	for _, pos := range content {
+		posInt, _ := strconv.Atoi(pos)
+		positions = append(positions, posInt)
+	}
+
+	// Identify most frequently used position (median)
+	sort.Ints(positions)
+	middle := len(positions) / 2
+	if len(positions)%2 != 0 {
+		median = positions[middle]
+	} else {
+		median = (positions[middle-1] + positions[middle]) / 2
+	}
+
+	// Calculate fuel consumption
+	for _, p := range positions {
+		if p == median {
+			continue
+		}
+		requiredFuel += int(math.Abs(float64(p - median)))
+	}
+
+	return strconv.Itoa(requiredFuel)
 }
 
 func (*day07) SolveB(input string) string {
-	return solution.NotImplemented
+	// input = `16,1,2,0,4,2,7,1,2,14`
+
+	content := strings.Split(input, ",")
+
+	var (
+		requiredFuel, sum, mean int
+		positions               = make([]int, 0, len(content))
+	)
+
+	// Convert to ints
+	for _, pos := range content {
+		posInt, _ := strconv.Atoi(pos)
+		positions = append(positions, posInt)
+	}
+
+	// Calculate mean position
+	for _, pos := range positions {
+		sum += pos
+	}
+	mean = int(math.Floor(float64(sum) / float64(len(positions))))
+
+	// Arithmetic progression function
+	calculateFuel := func(n int) int {
+		return (n * (1 + n)) / 2
+	}
+
+	// Calculate minimal fuel consumption
+	for _, p := range positions {
+		if p == mean {
+			continue
+		}
+		distance := int(math.Abs(float64(p - mean)))
+		requiredFuel += calculateFuel(distance)
+	}
+
+	return strconv.Itoa(requiredFuel)
 }
