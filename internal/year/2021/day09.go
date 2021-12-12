24072204aec35844feda4594ceba03a73608ff18
Sergej Voronov <sergej@voronov.lt>
2021-12-12 17:41:49 +0200

    Add 2021 day 9 solution

--- internal/year/2021/day09.go
+++ internal/year/2021/day09.go
@@ -1,6 +1,10 @@
 package aoc2021
 
 import (
+	"sort"
+	"strconv"
+	"strings"
+
 	"github.com/sergejvoronov/advent-of-code/internal/solution"
 )
 
@@ -11,9 +15,112 @@ func Day09() solution.Provider {
 }
 
 func (*day09) SolveA(input string) string {
-	return solution.NotImplemented
+	var answer int
+
+	lines := strings.Split(input, "\n")
+	matrix := make(map[int][]int)
+
+	for y, line := range lines {
+		for _, val := range line {
+			intVal, _ := strconv.Atoi(string(val))
+			matrix[y] = append(matrix[y], intVal)
+		}
+	}
+
+	for y := 0; y < len(lines); y++ {
+		for x := 0; x < len(matrix[y]); x++ {
+			if x > 0 {
+				if matrix[y][x-1] <= matrix[y][x] {
+					continue
+				}
+			}
+			if x < len(matrix[y])-1 {
+				if matrix[y][x+1] <= matrix[y][x] {
+					continue
+				}
+			}
+			if y > 0 {
+				if matrix[y-1][x] <= matrix[y][x] {
+					continue
+				}
+			}
+			if y < len(lines)-1 {
+				if matrix[y+1][x] <= matrix[y][x] {
+					continue
+				}
+			}
+
+			answer += matrix[y][x] + 1
+		}
+	}
+
+	return strconv.Itoa(answer)
 }
 
 func (*day09) SolveB(input string) string {
-	return solution.NotImplemented
+	var basins []int
+
+	lines := strings.Split(input, "\n")
+	matrix := make(map[int][]int)
+
+	for y, line := range lines {
+		for _, val := range line {
+			intVal, _ := strconv.Atoi(string(val))
+			matrix[y] = append(matrix[y], intVal)
+		}
+	}
+
+	for y := 0; y < len(lines); y++ {
+		for x := 0; x < len(matrix[y]); x++ {
+			if x > 0 {
+				if matrix[y][x-1] <= matrix[y][x] {
+					continue
+				}
+			}
+			if x < len(matrix[y])-1 {
+				if matrix[y][x+1] <= matrix[y][x] {
+					continue
+				}
+			}
+			if y > 0 {
+				if matrix[y-1][x] <= matrix[y][x] {
+					continue
+				}
+			}
+			if y < len(lines)-1 {
+				if matrix[y+1][x] <= matrix[y][x] {
+					continue
+				}
+			}
+
+			_, count := exploreBasins(matrix, 0, y, x)
+
+			basins = append(basins, count)
+		}
+	}
+
+	sort.Ints(basins)
+
+	return strconv.Itoa(basins[len(basins)-1] * basins[len(basins)-2] * basins[len(basins)-3])
+}
+
+func exploreBasins(matrix map[int][]int, count, y, x int) (map[int][]int, int) {
+	if matrix[y][x] != 9 {
+		count++
+		matrix[y][x] = 9
+		if y != len(matrix)-1 {
+			matrix, count = exploreBasins(matrix, count, y+1, x)
+		}
+		if y != 0 {
+			matrix, count = exploreBasins(matrix, count, y-1, x)
+		}
+		if x != len(matrix[y])-1 {
+			matrix, count = exploreBasins(matrix, count, y, x+1)
+		}
+		if x != 0 {
+			matrix, count = exploreBasins(matrix, count, y, x-1)
+		}
+	}
+
+	return matrix, count
 }
